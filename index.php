1a644a3912a573c405de10bb36aa28c61f298d54
Samuel Giles <aphro.cag@googlemail.com>
2012-03-14 20:56:26 +0000

    Initial benchmarking tests, added simple read write test for php sessions.

--- index.php
+++ index.php
@@ -42,3 +42,55 @@ function total() {
   $pad = str_repeat(" ", 24-strlen("Total")-strlen($num));
   echo "Total".$pad.$num."\n";
 }
+
+/**
+ * Benchmarks
+ */
+
+function sessionwrite($n) {
+  $i = -1;
+  while ($i++ < $n) {
+    if (!isset($_SESSION[$i])) {
+      $_SESSION[$i] = $i;
+    }
+  }
+}
+
+function sessionread($n) {
+  $i = -1;
+  while ($i++ < $n) {
+    if (isset($_SESSION[$i])) {
+      $f = $_SESSION[$i];
+    }
+  }
+}
+
+/**
+ * Test functions  - ?phpsess  <-  QS php sessions
+ */
+
+function readtests() {
+  $t0 = $t = start_test();
+  sessionread(100);
+  $t = end_test($t, "sessionread(100)");
+  total($t0, "Total");
+}
+
+function writetests() {
+  $t0 = $t = start_test();
+  sessionwrite(100);
+  $t = end_test($t, "sessionwrite(100)");
+  total($t0, "Total");
+}
+
+
+/**
+ * Run tests
+ */
+if (isset($_GET['phpsess']) && isset($_GET['write'])) {
+  session_start();
+  writetests();
+} else if (isset($_GET['phpsess']) && isset($_GET['read'])) {
+  session_start();
+  readtests();
+}
